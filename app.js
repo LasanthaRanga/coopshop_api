f07dbe839b79e621824bc3c87b4034b5917cc500
Ranga Rathnayake <35551906+LasanthaRanga@users.noreply.github.com>
2020-05-30 20:16:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const allowedOrigins = [
     'ionic://localhost',
     'http://localhost',
     'http://localhost:8080',
-    'http://localhost:8100'
+    'http://localhost:8100',
+    '*'
 ];
 
 // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
